99f527c233273b3e388412a7866c91ce02886e87
devies2 <68522356+devies2@users.noreply.github.com>
2020-09-10 17:09:34 +0900

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,17 @@
+(function($){
+    $('#containerBox').load('main.html');
+    
+    $('.link-set a').on('click', function(e){
+        e.preventDefault();
+        var url = $(this).attr('href');
+        $('#container').remove();
+        $('#containerBox').load('url');
+    })
+
+    $('.logo_nav h1 a').on('click',function(e){
+        e.preventDefault();
+        var url = $(this).attr('href');
+        $('#container').remove();
+        $('#containerBox').load('url')
+    })
+})(jQuery)
